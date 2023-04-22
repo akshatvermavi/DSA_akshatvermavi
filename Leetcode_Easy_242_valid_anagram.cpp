b08375bfae78da3544d0d8ce2a3acc639bf3e3fc
Akshat verma <99114167+akshatvermavi@users.noreply.github.com>
2023-04-22 18:59:43 +0530

    Leetcode_Easy_242_valid_anagram

---
+++ Leetcode_Easy_242_valid_anagram.cpp
@@ -0,0 +1,41 @@
+//Method 01 via sorting the string TC = O(NlogN) sc = O(1)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      sort(s.begin(),s.end());
+      sort(t.begin(),t.end());
+      for(int i=0;i<s.size();i++)
+      {
+          if(s[i]!=t[i])
+          return false;
+      }  
+      if(s.size()!=t.size())
+      return false;
+
+      return true;
+    }
+};
+
+// method 02 via frequency table of freqTable[256] = {0}; TC = O(M+N)  SC= O(256) constant sc
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      int freqTable[256] = {0};
+      for(int i=0;i<s.size();i++)
+      {
+        freqTable[s[i]]++;
+      }
+      for(int i=0;i<t.size();i++)
+      {
+        freqTable[t[i]]--;
+      }
+      for(int i=0;i<256;i++)
+      {
+        if(freqTable[i]!=0)
+        return false;
+      }
+      return true;
+    }
+};
