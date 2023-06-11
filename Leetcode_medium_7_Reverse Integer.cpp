3a61eee05e14d157a373be4838745a23f40ca7a9
Akshat verma <99114167+akshatvermavi@users.noreply.github.com>
2023-06-11 22:09:15 +0530

    Create Leetcode_medium_7_Reverse Integer

---
+++ Leetcode_medium_7_Reverse Integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+      
+       while(x!=0)
+       {
+           int dig = x%10;
+            x  = x / 10;
+           if((ans> INT_MAX/10) || (ans< INT_MIN/10))
+           return 0;
+           
+           ans = (ans*10) + dig;
+         
+       } 
+       
+       return ans;
+    }
+};
