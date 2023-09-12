b4b756a5de4f5b20a7df40c4bba7a680850e3eff
Akshat verma <99114167+akshatvermavi@users.noreply.github.com>
2023-09-12 21:00:53 +0530

    Create Gfg_easy_Perfect_Numbers

---
+++ Gfg_easy_Perfect_Numbers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int isPerfectNumber(long long N) {
+        // code here
+        // if(N==1) return 0;
+        // long sum =1;
+        // for(long i=2;i*i<=N;i++)
+        // {
+        //     if(N%i==0)
+        //     sum+=i;
+        //     //if(i!=N/i)sum = sum + N/i;
+        // }
+        // if(sum==N) return 1;
+        // return 0;
+        
+         if(N==1) return 0;
+        long sum = 1;
+        for(long i=2; i*i<=N; i++){
+            if(N%i==0){
+                sum = sum + i;
+                if(i!=N/i) sum = sum + N/i;
+            }
+        }
+        if(sum==N) return 1;
+        return 0;
+    }
+};
