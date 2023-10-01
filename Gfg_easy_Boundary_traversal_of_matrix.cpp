ff051634bddc9d5e088dd1048522303afe9875e3
Akshat verma <99114167+akshatvermavi@users.noreply.github.com>
2023-10-02 02:48:39 +0530

    Create Gfg_easy_Boundary_traversal_of_matrix

---
+++ Gfg_easy_Boundary_traversal_of_matrix.cpp
@@ -0,0 +1,32 @@
+class Solution
+{   
+    public:
+    //Function to return list of integers that form the boundary 
+    //traversal of the matrix in a clockwise manner.
+    vector<int> boundaryTraversal(vector<vector<int> > matrix, int n, int m) 
+    {
+        // code here
+                vector<int> vec;
+        for(int i=0; i<m; i++){
+            vec.push_back(matrix[0][i]);
+        }
+        for(int i=1; i<n-1; i++){
+            vec.push_back(matrix[i][m-1]);
+        }
+        if (n == 1)
+    {
+        return vec;
+    }
+        for(int i=m-1; i>=0; i--){
+            vec.push_back(matrix[n-1][i]);
+        }
+        if (m == 1)
+    {
+        return vec;
+    }
+        for(int i=n-2; i>0; i--){
+            vec.push_back(matrix[i][0]);
+        }
+        return vec;
+    }
+};
